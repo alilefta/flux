5e4fbfcd6282ade456360663f98046e49a887160
Ali Lefta <alilefta95@gmail.com>
2026-02-11 22:03:30 +0300

    extended errors to have channel specific messages

--- lib/errors.ts
+++ lib/errors.ts
@@ -18,4 +18,7 @@ export const ERRORS = {
 	NOT_MEMBER: new ActionError("You are not a member of this server", "NOT_MEMBER", 403),
 	CHANNEL_NOT_FOUND: new ActionError("Channel not found", "CHANNEL_NOT_FOUND", 404),
 	INVALID_INPUT: new ActionError("Invalid input", "INVALID_INPUT", 400),
+	CHANNEL_NAME_MUST_NOT_BE_GENERAL: new ActionError("Channel name cannot be 'general'", "INVALID_NAME", 400),
+	UNAUTHORIZE_ONLY_ADMINS_OR_MODERATORS: new ActionError("Unauthorized: Only Admins or Moderators can create channels", "UNAUTHORIZED", 401),
+	UNABLE_TO_REMOVE_DEFAULT_GENERAL_CHANNEL: new ActionError("Invalid Operation: Unable to remove the default server channel.", "INVALID_OPERATION", 401),
 } as const;
