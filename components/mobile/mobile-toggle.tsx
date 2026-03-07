149eee6e787a4c1a3a11411c6a35c254a5574b51
Ali Lefta <alilefta95@gmail.com>
2026-03-07 22:10:17 +0300

    fixed mobile menu and added custom close button

--- components/mobile/mobile-toggle.tsx
+++ components/mobile/mobile-toggle.tsx
@@ -1,5 +1,5 @@
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { ChevronLeft, Menu } from "lucide-react";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 import { ServerSidebar } from "@/components/server/server-sidebar";
@@ -55,14 +55,23 @@ export const MobileToggle = async ({ serverId }: MobileToggleProps) => {
 					<Menu className="w-5 h-5" />
 				</Button>
 			</SheetTrigger>
-			<SheetContent side="left" className="p-0 flex gap-0 w-[320px] bg-[#1e1e22] border-none">
-				{/* Navigation Rail (Always Visible) */}
-				<div className="w-[72px]">
-					<NavigationSidebar currentProfileId={profile.id} servers={servers} activeServerId={serverId} />
-				</div>
+			<SheetContent showCloseButton={false} side="left" className="p-0 gap-0 w-84 bg-[#1e1e22] border-none">
+				<SheetHeader className="sr-only">
+					<SheetTitle className="sr-only">Mobile Navigation</SheetTitle>
+					<SheetDescription className="sr-only">Mobile Navigation Menu</SheetDescription>
+				</SheetHeader>
+				<div className="flex w-full gap-0 flex-1 relative">
+					<SheetClose className="absolute z-0 top-2 -right-4.5 bg-[#1e1e22] p-2 rounded-full">
+						<ChevronLeft className="w-4 h-4" />
+					</SheetClose>
+					{/* Navigation Rail (Always Visible) */}
+					<div className="w-18">
+						<NavigationSidebar currentProfileId={profile.id} servers={servers} activeServerId={serverId} />
+					</div>
 
-				{/* Context Sidebar (Server or DM) */}
-				<div className="flex-1">{contextSidebar}</div>
+					{/* Context Sidebar (Server or DM) */}
+					<div className="flex-1">{contextSidebar}</div>
+				</div>
 			</SheetContent>
 		</Sheet>
 	);
