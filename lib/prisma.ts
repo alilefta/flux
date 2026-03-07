f46f99fe470a2d4948338d030c8b05ad27e6b0fd
Ali Lefta <alilefta95@gmail.com>
2026-03-07 22:16:32 +0300

    fixed singleton instance that wasn't used for development

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,18 +2,22 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
 const prismaClientSingleton = () => {
 	const adapter = new PrismaPg({ connectionString });
-	const prisma = new PrismaClient({ adapter });
-	return prisma;
+	return new PrismaClient({ adapter });
 };
 
-declare const globalThis: {
-	prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+declare global {
+	var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+}
+
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
-if (process.env.NODE_ENV === "production") globalThis.prismaGlobal = prisma;
+
+// cache in development
+if (process.env.NODE_ENV !== "production") {
+	globalThis.prismaGlobal = prisma;
+}
