6584a359dd5123cb784b60799acbe3876656e652
Ali Lefta <alilefta95@gmail.com>
2026-03-07 22:17:06 +0300

    used proper architecture for modal provider which resulted in better performance

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -14,33 +14,66 @@ import { UserSettingsModal } from "@/components/modals/user/user-settings-modal"
 import { MessageFileModal } from "@/components/modals/chat/message-file-modal";
 import { ChatSearchModal } from "@/components/modals/chat/chat-search-modal";
 import { useIsMounted } from "@/hooks/use-is-mounted";
+import { useModal } from "@/hooks/use-modal-store";
 
 export const ModalProvider = () => {
 	const isMounted = useIsMounted();
+	const type = useModal((state) => state.type);
 
 	if (!isMounted) return null;
 
-	return (
-		<>
-			{/* Server */}
-			<CreateServerModal />
-			<InviteModal />
-			<EditServerModal />
-			<LeaveServerModal />
-			<DeleteServerModal />
-			{/* Members  */}
-			<MembersModal />
-			<UserProfileModal />
-			{/* Channels */}
-			<CreateChannelModal />
-			<EditChannelModal />
-			<DeleteChannelModal />
-			{/* Users */}
-			<UserSettingsModal />
-			<MessageFileModal />
+	switch (type) {
+		case "createServer":
+			return <CreateServerModal />;
+		case "invite":
+			return <InviteModal />;
+		case "editServer":
+			return <EditServerModal />;
+		case "leaveServer":
+			return <LeaveServerModal />;
+		case "deleteServer":
+			return <DeleteServerModal />;
+		case "members":
+			return <MembersModal />;
+		case "userProfile":
+			return <UserProfileModal />;
+		case "userSettings":
+			return <UserSettingsModal />;
+		case "createChannel":
+			return <CreateChannelModal />;
+		case "editChannel":
+			return <EditChannelModal />;
+		case "deleteChannel":
+			return <DeleteChannelModal />;
+		case "searchMessages":
+			return <ChatSearchModal />;
+		case "messageFile":
+			return <MessageFileModal />;
+		default:
+			return null;
+	}
 
-			{/* Messages */}
-			<ChatSearchModal />
-		</>
-	);
+	// return (
+	// 	<>
+	// 		{/* Server */}
+	// 		<CreateServerModal />
+	// 		<InviteModal />
+	// 		<EditServerModal />
+	// 		<LeaveServerModal />
+	// 		<DeleteServerModal />
+	// 		{/* Members  */}
+	// 		<MembersModal />
+	// 		<UserProfileModal />
+	// 		{/* Channels */}
+	// 		<CreateChannelModal />
+	// 		<EditChannelModal />
+	// 		<DeleteChannelModal />
+	// 		{/* Users */}
+	// 		<UserSettingsModal />
+	// 		<MessageFileModal />
+
+	// 		{/* Messages */}
+	// 		<ChatSearchModal />
+	// 	</>
+	// );
 };
